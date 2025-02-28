a20fabd8c12d275d649a0dbe4b5b516ea84fd99b
marcosferrazdev <marcosvitorferraz4@gmail.com>
2025-02-28 12:49:11 -0300

    fix: bg in patientList

--- src/pages/PatientList.tsx
+++ src/pages/PatientList.tsx
@@ -47,96 +47,101 @@ function PatientList() {
   };
 
   return (
-    <div className="container mx-auto p-6">
-      <button
-        onClick={() => navigate("/home")}
-        className="flex items-center text-blue-600 hover:text-blue-800 mb-4"
-      >
-        <ArrowLeft className="w-5 h-5 mr-2" /> Voltar
-      </button>
-      <h2 className="text-2xl font-bold mb-4">Lista de Pacientes</h2>
+    <div className="min-h-screen bg-gray-100 flex flex-col py-4 px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto">
+        <button
+          onClick={() => navigate("/home")}
+          className="flex items-center text-blue-600 hover:text-blue-800 mb-4"
+        >
+          <ArrowLeft className="w-5 h-5 mr-2" /> Voltar
+        </button>
+        <h2 className="text-2xl sm:text-3xl font-bold text-gray-900 mb-6">
+          Lista de Pacientes
+        </h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {patients.map((patient) => (
-          <div
-            key={patient.id}
-            className="bg-white p-6 rounded-lg shadow-md flex flex-col justify-between"
-          >
-            <div>
-              <h3 className="mb-3 text-xl font-bold text-gray-900">
-                {patient.name}
-              </h3>
-              <p className="text-gray-700">
-                <span className="font-semibold">Telefone:</span> {patient.phone}
-              </p>
-              <p className="text-gray-700">
-                <span className="font-semibold">Cidade:</span>{" "}
-                {patient.recruitmentCity}
-              </p>
-              <p className="text-gray-700">
-                <span className="font-semibold">Diagnóstico:</span>{" "}
-                {patient.diagnosis}
-              </p>
-              <p className="text-gray-700">
-                <span className="font-semibold">Fase:</span> {patient.phase}
-              </p>
-              <p className="text-gray-700">
-                <span className="font-semibold">Próx. Coleta:</span>{" "}
-                {formatDate(patient.nextCollectionDate)}
-              </p>
-              <p className="text-gray-700">
-                <span className="font-semibold">Comentário:</span>{" "}
-                {patient.callComments}
-              </p>
-              <p className="text-gray-700">
-                <span className="font-semibold">Transporte:</span>{" "}
-                {patient.transportation}
-              </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {patients.map((patient) => (
+            <div
+              key={patient.id}
+              className="bg-white p-6 rounded-lg shadow-md flex flex-col justify-between"
+            >
+              <div>
+                <h3 className="mb-3 text-xl font-bold text-gray-900">
+                  {patient.name}
+                </h3>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Telefone:</span>{" "}
+                  {patient.phone}
+                </p>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Cidade:</span>{" "}
+                  {patient.recruitmentCity}
+                </p>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Diagnóstico:</span>{" "}
+                  {patient.diagnosis}
+                </p>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Fase:</span> {patient.phase}
+                </p>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Próx. Coleta:</span>{" "}
+                  {formatDate(patient.nextCollectionDate)}
+                </p>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Comentário:</span>{" "}
+                  {patient.callComments}
+                </p>
+                <p className="text-gray-700">
+                  <span className="font-semibold">Transporte:</span>{" "}
+                  {patient.transportation}
+                </p>
+              </div>
+              <div className="flex justify-between mt-4">
+                <button
+                  onClick={() => handleDeleteClick(patient.id)}
+                  className="text-red-600 hover:text-red-800"
+                  title="Excluir"
+                >
+                  <Trash2 className="w-6 h-6" />
+                </button>
+                <Link
+                  to={`/editar/${patient.id}`}
+                  className="text-blue-600 hover:text-blue-800"
+                  title="Editar"
+                >
+                  <Edit className="w-6 h-6" />
+                </Link>
+              </div>
             </div>
-            <div className="flex justify-between mt-4">
-              <button
-                onClick={() => handleDeleteClick(patient.id)}
-                className="text-red-600 hover:text-red-800"
-                title="Excluir"
-              >
-                <Trash2 className="w-6 h-6" />
-              </button>
-              <Link
-                to={`/editar/${patient.id}`}
-                className="text-blue-600 hover:text-blue-800"
-                title="Editar"
-              >
-                <Edit className="w-6 h-6" />
-              </Link>
-            </div>
-          </div>
-        ))}
-      </div>
-
-      {/* Modal de Confirmação de Exclusão */}
-      {showDeleteModal && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h3 className="text-lg font-bold mb-4">
-              Tem certeza que deseja excluir?
-            </h3>
-            <div className="flex justify-end gap-4">
-              <button
-                onClick={() => setShowDeleteModal(false)}
-                className="px-4 py-2 bg-gray-400 text-white rounded hover:bg-gray-500"
-              >
-                Cancelar
-              </button>
-              <button
-                onClick={confirmDeletePatient}
-                className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
-              >
-                Excluir
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
-      )}
+
+        {/* Modal de Confirmação de Exclusão */}
+        {showDeleteModal && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white p-6 rounded-lg shadow-lg">
+              <h3 className="text-lg font-bold mb-4">
+                Tem certeza que deseja excluir?
+              </h3>
+              <div className="flex justify-end gap-4">
+                <button
+                  onClick={() => setShowDeleteModal(false)}
+                  className="px-4 py-2 bg-gray-400 text-white rounded hover:bg-gray-500"
+                >
+                  Cancelar
+                </button>
+                <button
+                  onClick={confirmDeletePatient}
+                  className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
+                >
+                  Excluir
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
